64728db2396687479415e5fb45da3275010e8603
Paolo <wildlux@gmail.com>
2025-08-13 22:41:27 +0200

    Update __init__.py

--- Versione_1/__init__.py
+++ Versione_1/__init__.py
@@ -1,12 +1,19 @@
 
 ''''
 Cartella principale/
-└── assistente_dsa/
+└─── assistente_dsa/
     ├── __init__.py
     ├── main_app.py
     ├── ollama_manager.py
     ├── speech_recognition_manager.py
     ├── tts_manager.py
-    └── visual_background.py
+    ├── visual_background.py
+    ├── settings.json
+    ├── output.mp3
+    ├── opencv_recognizer.py
+    ├── vosk_model_manager
+    └── media_pipe_recognizer.py ( to implement )
+
 '''
 #print ("ciao")
+
